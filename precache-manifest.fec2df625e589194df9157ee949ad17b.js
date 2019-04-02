25a6d6420d79e887f10b5cbbccecd8730e60dbb5
sarahcheatham <sarah.cheatham14@gmail.com>
2019-04-02 16:04:38 -0500

    Updates

---
+++ precache-manifest.fec2df625e589194df9157ee949ad17b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "58beda0c78fa0b567a03",
+    "url": "/sarahcheatham/static/css/main.4ad8f460.chunk.css"
+  },
+  {
+    "revision": "58beda0c78fa0b567a03",
+    "url": "/sarahcheatham/static/js/main.03254a70.chunk.js"
+  },
+  {
+    "revision": "8545d77276c6370160ca",
+    "url": "/sarahcheatham/static/js/runtime~main.9a98f474.js"
+  },
+  {
+    "revision": "d1ca15c337a44d8b6fe3",
+    "url": "/sarahcheatham/static/js/2.4dcc5f8e.chunk.js"
+  },
+  {
+    "revision": "2a741a87a72cccc238663269722517ce",
+    "url": "/sarahcheatham/static/media/gravatar.2a741a87.JPG"
+  },
+  {
+    "revision": "50a675f1cec2b4e6acb94db86b7de7d8",
+    "url": "/sarahcheatham/static/media/arraynge.50a675f1.png"
+  },
+  {
+    "revision": "f65adea0421207a1d4895a10c87eb0cf",
+    "url": "/sarahcheatham/static/media/method.f65adea0.png"
+  },
+  {
+    "revision": "d7a449b09411e8dab5703f112c64ae54",
+    "url": "/sarahcheatham/static/media/todolist.d7a449b0.png"
+  },
+  {
+    "revision": "baa59224ec3c8f6f903fd5ada6cb4326",
+    "url": "/sarahcheatham/index.html"
+  }
+];
