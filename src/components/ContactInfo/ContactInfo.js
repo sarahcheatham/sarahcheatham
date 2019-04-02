b3e57b13c5403726cb2ffe316adc61f1584e796b
sarahcheatham <sarah.cheatham14@gmail.com>
2019-04-02 11:39:54 -0500

    uploading resume

--- src/components/ContactInfo/ContactInfo.js
+++ src/components/ContactInfo/ContactInfo.js
@@ -6,10 +6,9 @@ class ContactInfo extends React.Component{
         return (
             <div className="contactinfo">
                 <h1 id="contactinfoheader">Contact Info:</h1>
-                <p id="resume">PDF Resume</p>
+                <h2 id="resume" className="linkHeader">PDF Resume</h2>
                 <p id="email">Email: sarah.cheatham14@gmail.com</p>
                 <p id="phone">Phone: (281) 961 - 6790</p>
-                {/* <p id="resume">PDF Resume</p> */}
                 {/* <h2 className="linkHeader">
                     <a href="https://github.com/sarahcheatham">sarah cheatham</a>
                 </h2> */}
