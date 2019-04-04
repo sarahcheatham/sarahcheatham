8ea7d11c183961e3a3e1a9cc947d02b6e0f7b251
sarahcheatham <sarah.cheatham14@gmail.com>
2019-04-04 11:23:16 -0500

    fixed overlapping divs

--- src/components/Projects/ProjectsHeader.js
+++ src/components/Projects/ProjectsHeader.js
@@ -4,9 +4,9 @@ import "./Projects.css";
 class ProjectHeader extends React.Component{
     render(){
         return(
-            <div className="workcontainer">
-                <div id="yellowBar"></div>
+            <div className="projectsContainer">
                 <h1 id="projectsH1">projects.</h1>
+                <div id="yellowBar"></div>
             </div>
         );
     }
--- src/components/Work/WorkHeader.js
+++ src/components/Work/WorkHeader.js
@@ -5,8 +5,8 @@ class WorkHeader extends React.Component{
     render(){
         return(
             <div className="work">
-                <div id="pinkBar"></div>
                 <h1 id="workH1">work.</h1>
+                <div id="pinkBar"></div>
             </div>
         );
     }
